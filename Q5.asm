4b86d5607e848de349e4f702515ea99a17381c84
vaanyabajaj <vaanyabajaj8@gmail.com>
2026-02-05 21:45:23 +0530

    Create Q5.asm

---
+++ Q5.asm
@@ -0,0 +1,32 @@
+ORG 0000H
+
+MOV R0, #40H       
+MOV R1, #40H     
+
+SCAN:
+MOV A, @R0       
+CJNE A, #0FFH, COPY
+SJMP NEXT        
+
+COPY:
+MOV @R1, A        
+INC R1           
+
+NEXT:
+INC R0            
+CJNE R0, #60H, SCAN
+
+
+FILL:
+MOV A, R1
+CJNE A, #60H, FILL_ZERO
+SJMP END
+
+FILL_ZERO:
+MOV @R1, #00H
+INC R1
+SJMP FILL
+
+END:
+SJMP END
+END
